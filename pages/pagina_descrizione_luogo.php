88b5562430b60c27a9a5338440ddf355064bdd76
Ghilker <minefap44@gmail.com>
2022-12-30 15:10:09 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,14 +1,12 @@
 
-<div class="bg-image">
-  <!-- content here -->
-
-
+<html>
+<body style='background-image: url(\"imgs/pattern.png\")'>
+<h1>Descrizione dettagliata luogo</h1>
 <?php
-require '../ref_header.inc.php';
-require '../footer.inc.php';
-
 session_start();
 
 echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];
 
-?></div>
+?>
+
+</html>
