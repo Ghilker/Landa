02ffc4a24cd89a2a6f38184f1033f7a935260fc2
Ghilker <minefap44@gmail.com>
2022-12-30 22:18:06 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,6 +1,11 @@
 <head>
   <link rel="stylesheet" type="text/css" href="../themes/tok1/descrizione_luogo_dettagliata.css">
 </head>
+<body>
+    <div id = "container">
+        Descrizione dettagliata luogo
+    </div>
+</body>
 <?php
 session_start();
 echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];?>
