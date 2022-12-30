3e8588b826204125d38823c0af60f2734e33cff3
Ghilker <minefap44@gmail.com>
2022-12-30 15:16:31 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,12 +1,9 @@
 
 <html>
-<body style='background-image: url("imgs/pattern.png")'>
-<h1>Descrizione dettagliata luogo</h1>
-<?php
-session_start();
-
-echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];
-
-?>
-
+    <body style='background-image: url("imgs/pattern.png")'>
+    <h1>Descrizione dettagliata luogo</h1>
+    <?php
+    session_start();
+    echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];?>
+    </body>
 </html>
