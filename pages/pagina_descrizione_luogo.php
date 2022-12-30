2d31a0e58746233ae8843b388dac1979b011f7c7
Ghilker <minefap44@gmail.com>
2022-12-30 22:05:38 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,15 +1,21 @@
 <html>
-  <body>
+  <body style='background-image: url("../themes/tok1/imgs/pattern.png"); display: flex; align-items: flex-start; position: relative;'>
     <div style="text-align:center;">
-      <img src="../imgs/sfondopg.png" alt="" style="margin: 0 auto;">
-      <div style="position:absolute; top:50%; left:50%; transform:translate(-50%, -50%);">
+      <img src="../imgs/sfondopg.png" alt="" style="margin: 0 auto; max-width: 100%; max-height: 100%;">
+      <div style="position:absolute;">
         Descrizione dettagliata luogo
       </div>
     </div>
   </body>
 </html>
-<?php
-session_start();
-echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];?>
 
+<html>
+    <body style='background-image: url("../themes/tok1/imgs/pattern.png"); display: flex; align-items: flex-start; position: relative;'>
+        <img src="../imgs/sfondopg.png" style='display: block; max-width: 100%; max-height: 100%; position: absolute; top: 0;'>
+        <div style='position: absolute; z-index: 1; text-align: center;'>Descrizione dettagliata luogo</div>
+    <?php
+    session_start();
+    echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];?>
+    </body>
+</html>
 
