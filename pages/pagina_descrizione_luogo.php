06f68716749b3f74276bcdaca047c101ae689c7a
Ghilker <minefap44@gmail.com>
2022-12-30 15:10:26 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,6 +1,6 @@
 
 <html>
-<body style='background-image: url(\"imgs/pattern.png\")'>
+<body style='background-image: url("imgs/pattern.png")'>
 <h1>Descrizione dettagliata luogo</h1>
 <?php
 session_start();
