d5ec0a93dcab638966c6d60f756979c7e37653cb
Ghilker <minefap44@gmail.com>
2022-12-30 10:53:53 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,6 +1,8 @@
 <head>
-	<link rel='stylesheet' href='themes/<?php echo $PARAMETERS['themes']['current_theme']; ?>/main.css' TYPE='text/css'>
-</head>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+		<link rel='stylesheet' href='themes/<?php echo $PARAMETERS['themes']['current_theme']; ?>/main.css' TYPE='text/css'>
+		<link rel='shortcut icon' href='favicon.ico' />
+	</head>
 
 <?php
 require '../ref_header.inc.php';
