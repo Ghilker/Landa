f86b8227415cdf0e564973c55cc1ef57ca3b1290
Ghilker <minefap44@gmail.com>
2022-12-30 13:53:54 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,16 +1,15 @@
+
+<html>
 <head>
-		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
-		<link rel='stylesheet' href='themes/<?php echo $PARAMETERS['themes']['current_theme']; ?>/main.css' TYPE='text/css'>
-		<link rel='shortcut icon' href='favicon.ico' />
-	</head>
+	<meta http-equiv="Content-Type" content='text/html; charset=utf-8'>
+	<link rel="stylesheet" href="themes/<?php echo $PARAMETERS['themes']['current_theme']; ?>/descrizione_luogo_dettagliata.css" TYPE='text/css'>
+</head>
+</html>
 
 <?php
 require '../ref_header.inc.php';
 require '../footer.inc.php';
 
-
-
-// Get the array from the query parameter
 session_start();
 
 echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];
