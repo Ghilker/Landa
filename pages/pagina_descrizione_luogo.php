19e1a4f0734137af7964748dba1513886a9af3f6
Ghilker <minefap44@gmail.com>
2022-12-30 22:00:36 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,7 +1,7 @@
 <html>
     <body style='background-image: url("../themes/tok1/imgs/pattern.png"); display: flex; align-items: flex-start; position: relative;'>
         <img src="../imgs/sfondopg.png" style='display: block; max-width: 100%; max-height: 100%; position: absolute; top: 0;'>
-        <h1 style='text-align: center; position: absolute; z-index: 1;'>Descrizione dettagliata luogo</h1>
+        <div style='position: absolute; z-index: 1; text-align: center;'>Descrizione dettagliata luogo</div>
     <?php
     session_start();
     echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];?>
