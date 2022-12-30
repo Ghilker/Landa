4d9779c97749d3b00461655ec1f76e77edf84cc0
Ghilker <minefap44@gmail.com>
2022-12-30 13:55:06 +0100

    trythis

--- pages/info_location.inc.php
+++ pages/info_location.inc.php
@@ -26,7 +26,7 @@
                 <marquee onmouseover="this.stop()" onmouseout="this.start()" direction="left" scrollamount="3"
                     class="stato_luogo">&nbsp;
                     <?php
-                    echo '<a href="#" onclick="window.open(\'../pages/pagina_descrizione_luogo.php\', \'Descrizione Luogo\', \'height=500,width=500\');">' . gdrcd_filter('out', $nome_luogo) . '</a>';
+                    echo '<a href="#" onclick="window.open(\'../main.php?page=forum\', \'Descrizione Luogo\', \'height=500,width=500\');">' . gdrcd_filter('out', $nome_luogo) . '</a>';
                     ?>
                 </marquee>
             </h2>
