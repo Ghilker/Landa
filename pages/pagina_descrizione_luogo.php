16979b47eaacff79637332bb11f66e0f0f0aecca
Ghilker <minefap44@gmail.com>
2022-12-30 10:35:53 +0100

    test

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,7 +1,9 @@
 <?php
+require '../ref_header.inc.php';
+require '../footer.inc.php';
 // Get the array from the query parameter
 session_start();
 
-echo $_SESSION['descrizione_luogo']['descrizione'];
+echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];
 
 ?>
