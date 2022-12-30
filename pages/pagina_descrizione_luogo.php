10bd7adaa67782a0a0f5a030001486049b078409
Ghilker <minefap44@gmail.com>
2022-12-30 10:52:32 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,6 +1,13 @@
+<head>
+	<link rel='stylesheet' href='themes/<?php echo $PARAMETERS['themes']['current_theme']; ?>/main.css' TYPE='text/css'>
+</head>
+
 <?php
 require '../ref_header.inc.php';
 require '../footer.inc.php';
+
+
+
 // Get the array from the query parameter
 session_start();
 
