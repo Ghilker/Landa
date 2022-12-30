a636784ed60212a4d5b0c1875a1250a3795f7706
Ghilker <minefap44@gmail.com>
2022-12-30 19:41:07 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -1,6 +1,6 @@
 <html>
     <body style='background-image: url("../themes/tok1/imgs/pattern.png"); display: flex; align-items: flex-start;'>
-        <img src="../imgs/sfondopg.png" style='display: block; width: 100%; height: 100%;'>
+        <img src="../imgs/sfondopg.png" style='display: block; max-width: 100%; max-height: 100%; position: absolute; top: 0;'>
         <h1 style='text-align: center;'>Descrizione dettagliata luogo</h1>
     <?php
     session_start();
