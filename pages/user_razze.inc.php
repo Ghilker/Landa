a84bea72607527dbeacbda900e367bc6b7e4724c
Ghilker <minefap44@gmail.com>
2022-12-31 17:54:37 +0100

    trythis

--- pages/user_razze.inc.php
+++ pages/user_razze.inc.php
@@ -6,15 +6,17 @@
 		<?php /*HELP: */
 
 
-		$query = "SELECT nome_razza, sing_m, sing_f, descrizione, url_site, immagine, icon  FROM razza WHERE visibile = 1 ORDER BY nome_razza";
-		$result = gdrcd_query($query, 'result');?>
+		$query1 = "SELECT nome_razza, sing_m, sing_f, descrizione, url_site, immagine, icon  FROM razza WHERE visibile = 1 ORDER BY nome_razza";
+		$result1 = gdrcd_query($query1, 'result');?>
 		<div class="panels_box">
 			<div class="elenco_record_gioco">
 				<?php 
-				foreach($result as $row){
+				foreach($result1 as $row){
 					$value = $row['nome_razza'];
 					echo "<button> $value </button>";
 				}
+				$query = "SELECT nome_razza, sing_m, sing_f, descrizione, url_site, immagine, icon  FROM razza WHERE visibile = 1 ORDER BY nome_razza";
+				$result = gdrcd_query($query, 'result');?>
 				?>
 				<table>
 					<?php while ($row = gdrcd_query($result, 'fetch')) { ?>
