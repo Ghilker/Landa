0a32901aed12f371f59b5fd3b43d4b0416cc5732
Ghilker <minefap44@gmail.com>
2022-12-31 14:54:01 +0100

    trythis

--- pages/pagina_descrizione_luogo.php
+++ pages/pagina_descrizione_luogo.php
@@ -6,40 +6,43 @@
             background-position: left top;
         }
 
+        #desc_wrapper {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
         #desc_container {
             background-image: url('/imgs/sfondopg.png');
             background-repeat: no-repeat;
             background-position: center;
             background-size: cover;
-            display: flex;
-            align-items: center;
-            justify-content: center;
             text-align: center;
         }
 
         #desc_output {
             width: auto;
             overflow: auto;
-            align-items: center;
-            justify-content: center;
             text-align: center;
             background: url("") no-repeat center #0a1228;
             color: #7b859f;
             font-size: 18px;
             font-family: Arial, sans-serif;
-            display: flex;
         }
     </style>
 </head>
 
 <body>
-    <div id="desc_container">
-        <p>Descrizione dettagliata luogo</p>
-    </div>
-    <div id="desc_output">
-        <p><?php
-        session_start();
-        echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];
-        ?><p>
+    <div id="desc_wrapper">
+        <div id="desc_container">
+            <p>Descrizione dettagliata luogo</p>
+        </div>
+        <div id="desc_output">
+            <p><?php
+            session_start();
+            echo $_SESSION['descrizione_luogo']['descrizione_dettagliata'];
+            ?>
+            <p>
+        </div>
     </div>
 </body>
