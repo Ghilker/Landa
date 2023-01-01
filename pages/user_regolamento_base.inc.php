54e5a4373d69eb797f688dfd160067b7c57d05c0
Ghilker <minefap44@gmail.com>
2023-01-01 12:46:00 +0100

    trythis

--- pages/user_regolamento_base.inc.php
+++ pages/user_regolamento_base.inc.php
@@ -3,30 +3,16 @@
 	<div style="margin-top: 15px; margin-bottom: 20px"><img src="/imgs/regolamento.png"></div>
 
 	<div class="page_body">
-		<!--ELENCO CLICCABILE-->
-		<div>
-			<p style="font-size:14px;">Indice</p>
-			<br>
-			1. <a
-				href="https://provalice.altervista.org/main.php?page=user_regolamento_base#:~:text=1)-,Istruzioni%20per%20l%27uso,-INDICE%3A%0A%0A1">
-				Istruzioni per l'uso </a><br>
-			2. <a
-				href="https://provalice.altervista.org/main.php?page=user_regolamento_base#:~:text=2)-,Regolamento Base,-INDICE%3A%0A%0A1">
-				Regolamento Base </a><br>
-			3. <a
-				href="https://provalice.altervista.org/main.php?page=user_regolamento_base#:~:text=3)-,Skills%20speciali,-SKILLS%20SPECIALI%0A%0ALe">
-				Skills speciali </a><br>
-			4. <a
-				href="https://provalice.altervista.org/main.php?page=user_regolamento_base#:~:text=4)-,Lavori%20e%20Mestieri,-LAVORI%20E%20MESTIERI">
-				Lavori e Mestieri </a><br>
-			5. <a
-				href="https://provalice.altervista.org/main.php?page=user_regolamento_base#:~:text=5)-,Gli%20Allineamenti,-GLI%20ALLINEAMENTI%0AL%E2%80%99allineamento">
-				Gli Allineamenti </a><br>
-			6. <a
-				href="https://provalice.altervista.org/main.php?page=user_regolamento_base#:~:text=6)-,Informazioni%20utili,-PARAMETRI%20VARI%20E">
-				Informazioni utili </a><br>
-		</div>
 		<?php /*HELP: */
+
+		$query1 = "SELECT articolo, titolo FROM regolamento_base ORDER BY articolo";
+		$result1 = gdrcd_query($query1, 'result');
+
+		foreach($result1 as $result1){
+			$value = $result1['articolo'];
+			$name = $result1['titolo'];
+			echo "<button id = button$value> $name </button>";
+		}
 
 
 		$query = "SELECT articolo, titolo, testo FROM regolamento_base ORDER BY articolo";
