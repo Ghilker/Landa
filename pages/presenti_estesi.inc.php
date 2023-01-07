f41a97b7122d59119335626a61630e297263dc3e
Ghilker <minefap44@gmail.com>
2023-01-07 14:35:50 +0100

    trythis

--- pages/presenti_estesi.inc.php
+++ pages/presenti_estesi.inc.php
@@ -234,7 +234,6 @@
 			}
 			echo '<td style="padding-left:15px; padding-right:15px; background-color: rgba(99, 109, 135, 0.15);">' . $gilda . '</td>'; /* Placeholder per icona del Lavoro */
 			
-			
 			// 7 Missiva
 			//Iconcina del messaggio cliccabile
 			?>
