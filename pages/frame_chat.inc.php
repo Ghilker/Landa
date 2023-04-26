4b586840a21eca25b6a6d4ccfc09304e744071f2
Ghilker <minefap44@gmail.com>
2023-04-26 22:35:20 +0200

    trythis

--- pages/frame_chat.inc.php
+++ pages/frame_chat.inc.php
@@ -21,6 +21,8 @@ $info = gdrcd_query("SELECT nome, stanza_apparente, invitati, privata, proprieta
 		//e' una stanza privata?
 		if ($info['privata'] == 1) {
 			$allowance = FALSE;
+			$test1 = $row['scadenza'];
+			$test2 = date('Y-m-d H:i:s');
 			if (
 				(($info['proprietario'] == gdrcd_capital_letter($_SESSION['login'])) ||
 					(strpos($_SESSION['gilda'], $info['proprietario']) != FALSE) ||
